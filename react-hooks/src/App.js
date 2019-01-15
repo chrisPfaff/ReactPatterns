1f5fb495fb65dbeea3eeef9f3581e242b9b162cf
Chris Pfaff <chrispfaff10@gmail.com>
2019-01-15 16:49:27 -0600

    created first hook

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,14 +1,31 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Todo from "./Todo";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">React Hooks</header>
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "React Hooks",
+      isCompleted: false
+    },
+    {
+      text: "Go Browns",
+      isCompleted: false
+    },
+    {
+      text: "Go Bucks",
+      isCompleted: false
+    }
+  ]);
+  return (
+    <div className="app">
+      <header className="app-header">React Hooks</header>
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
-
 export default App;
