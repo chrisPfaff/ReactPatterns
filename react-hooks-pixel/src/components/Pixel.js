f3732790f9528bcd8f07576cc4eeabbb1863c982
Chris Pfaff <chrispfaff10@gmail.com>
2019-01-31 17:00:20 -0600

    started refactor with a grid

--- react-hooks-pixel/src/components/Pixel.js
+++ react-hooks-pixel/src/components/Pixel.js
@@ -1,17 +1,5 @@
 import React from "react";
 
-const Pixel = props => {
-  const changeColor = event => {
-    console.log((event.target.style.background = `${props.getColor}`));
-  };
-
-  return (
-    <div
-      onClick={changeColor}
-      className="pixel"
-      style={{ background: `${props.color}` }}
-    />
-  );
+export default props => {
+  return <div className={`${props.background} pixel`} />;
 };
-
-export default Pixel;
