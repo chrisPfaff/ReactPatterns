985e502b9ccc7202477bc611f57d12543ea20695
Chris Pfaff <chrispfaff10@gmail.com>
2019-02-10 15:42:11 -0600

    added cardArray to card

--- black-jack-hooks/src/components/Player.js
+++ black-jack-hooks/src/components/Player.js
@@ -1,11 +1,6 @@
 import React from "react";
 
 const Player = props => {
-  const handleBeginClick = () => {
-    props.addNewDealerCard();
-    props.addNewPlayerCard();
-  };
-
   return (
     <div>
       <div className="player">{props.children}</div>
